39894419bcc3caf7708abc7175620a378a7ec947
Carlos Marquez <carl.marq95@gmail.com>
2018-06-20 17:04:11 -0600

    setup basic things so I can start re working my folio

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+		<!--Font Awesome-->
+		<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
+		<!--CSS Stylesheet-->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<!--JQuery first, Popper.js second, and Bootstrap Js  third-->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+				  integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+				  crossorigin="anonymous"></script>
+
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
+				  integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+				  crossorigin="anonymous"></script>
+
+		<!--Google Fonts-->
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Quicksand">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Julius+Sans+One">
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+
+		<!--form validate js-->
+		<script src="./js/form-validate.js"></script>
+
+		<title>Carlos Marquez, Junior Developer</title>
+
+	</head>
+		<body>
+			<!--Carlos-->
+			<h3>Carlos</h3>
+			<h3>Projects</h3>
+			<h3>Links</h3>
+			<h3>Contact</h3>
+	</body>
+</html>
